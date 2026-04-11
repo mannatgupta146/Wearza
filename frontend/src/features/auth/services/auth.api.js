357c5317fd2691162ebaf757651b92d15670d42d
mannatgupta146 <mannatgupta146@gmail.com>
2026-04-11 18:01:59 +0530

    created proxy

--- frontend/src/features/auth/services/auth.api.js
+++ frontend/src/features/auth/services/auth.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const authApi = axios.create({
-    baseURL: "http://localhost:3000/api/auth",
+    baseURL: "/api/auth",
     withCredentials: true
 })
 
@@ -26,7 +26,7 @@ export async function loginUser({ email, password }) {
     try {
         const response = await authApi.post("/login", { email, password });
         return response.data;
-        
+
     } catch (error) {
         throw error;
     }
