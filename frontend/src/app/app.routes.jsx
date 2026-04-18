4cc5f0b968dbd2b5bb2f6941543bf37bbe6adaed
mannatgupta146 <mannatgupta146@gmail.com>
2026-04-18 13:21:48 +0530

    added

--- frontend/src/app/app.routes.jsx
+++ frontend/src/app/app.routes.jsx
@@ -1,52 +1,52 @@
-import { createBrowserRouter } from "react-router-dom";
-import { Navigate } from "react-router-dom";
-import { useSelector } from "react-redux";
-import Login from "../features/auth/pages/Login";
-import Register from "../features/auth/pages/Register";
+import { createBrowserRouter } from "react-router-dom"
+import { Navigate } from "react-router-dom"
+import { useSelector } from "react-redux"
+import Login from "../features/auth/pages/Login"
+import Register from "../features/auth/pages/Register"
 
 const ProtectedRoute = ({ children }) => {
-    const user = useSelector((state) => state.auth.user)
+  const user = useSelector((state) => state.auth.user)
 
-    if (!user) {
-        return <Navigate to="/login" replace />
-    }
+  if (!user) {
+    return <Navigate to="/login" replace />
+  }
 
-    return children
+  return children
 }
 
 const PublicOnlyRoute = ({ children }) => {
-    const user = useSelector((state) => state.auth.user)
+  const user = useSelector((state) => state.auth.user)
 
-    if (user) {
-        return <Navigate to="/home" replace />
-    }
+  if (user) {
+    return <Navigate to="/home" replace />
+  }
 
-    return children
+  return children
 }
 
 export const routes = createBrowserRouter([
-    {
-        path: "/home",
-        element: (
-            <ProtectedRoute>
-                <h1>Home Page</h1>
-            </ProtectedRoute>
-        )
-    },
-    {
-        path: "/login",
-        element: (
-            <PublicOnlyRoute>
-                <Login />
-            </PublicOnlyRoute>
-        )
-    },
-    {
-        path: "/register",
-        element: (
-            <PublicOnlyRoute>
-                <Register />
-            </PublicOnlyRoute>
-        )
-    }
+  {
+    path: "/home",
+    element: (
+      <ProtectedRoute>
+        <h1>Home Page</h1>
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: "/login",
+    element: (
+      <PublicOnlyRoute>
+        <Login />
+      </PublicOnlyRoute>
+    ),
+  },
+  {
+    path: "/register",
+    element: (
+      <PublicOnlyRoute>
+        <Register />
+      </PublicOnlyRoute>
+    ),
+  },
 ])
