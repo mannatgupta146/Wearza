15e63ba44856dd21d5f9c93c1757c428d2ac7d6d
mannatgupta146 <mannatgupta146@gmail.com>
2026-04-22 19:41:25 +0530

    feat: implement cart redux slice and integrate into global store

---
+++ frontend/src/features/cart/state/cart.slice.js
@@ -0,0 +1,32 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+
+const cartSlice = createSlice({
+    name: "cart",
+    initialState: {
+        items: [],
+
+    },
+    reducers: {
+        setCart: (state, action) => {
+            state.items = action.payload
+        },
+        addItem: (state, action) => {
+            state.items.push(action.payload)
+        },
+        removeItem: (state, action) => {
+            state.items = state.items.filter(item => item._id !== action.payload)
+        },
+        updateItem: (state, action) => {
+            const { _id, quantity } = action.payload
+            const item = state.items.find(item => item._id === _id)
+            if (item) {
+                item.quantity = quantity
+            }
+        }
+    }
+})
+
+export const { setCart, addItem, removeItem, updateItem } = cartSlice.actions
+
+export default cartSlice.reducer    
